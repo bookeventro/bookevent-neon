4976fbc1e613c41fee204faa9dec7c8f7b6aeefc
bookeventro <bookevent.ro@gmail.com>
2025-11-23 18:53:44 +0200

    Create next-env.d.ts

---
+++ next-env.d.ts
@@ -0,0 +1,12 @@
+diff --git a/next-env.d.ts b/next-env.d.ts
+new file mode 100644
+index 0000000000000000000000000000000000000000..9bc3dd46b9d9b21099f578814407e8926706fb09
+--- /dev/null
++++ b/next-env.d.ts
+@@ -0,0 +1,6 @@
++/// <reference types="next" />
++/// <reference types="next/types/global" />
++/// <reference types="next/image-types/global" />
++
++// NOTE: This file should not be edited
++// see https://nextjs.org/docs/basic-features/typescript for more information.
